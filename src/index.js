9ec77c3555d1fe05267546ec25c16370de32f76b
QambarAli1 <qamberali4904@gmail.com>
2021-09-02 01:14:17 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import anonymous1 from './anonymous1.jpg'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Post(props){
+  return <div className='main-div'>
+    <div className='postDiv'>
+      <h3>{props.name}</h3>
+      <br />
+      <img src={props.src}></img>
+    </div>
+  </div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Post name='Qambar Ali' src={anonymous1} />,document.querySelector('#root'))
+
